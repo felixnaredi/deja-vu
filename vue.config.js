946355075bf17117e2a41f80545452cca7c76ee9
felixnaredi <naredi@student.chalmers.se>
2022-09-30 12:28:27 +0200

    Added rust wasm library

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {},
+  configureWebpack: {
+    plugins: [
+      new WasmPackPlugin({
+        crateDirectory: path.resolve(__dirname, "wasm"),
+        outDir: path.resolve(__dirname, "dist", "wasm"),
+      }),
+    ],
+    experiments: {
+      asyncWebAssembly: true,
+    },
+  },
 });
