6080d4815af4bf99000aa834aecae2619dff37b9
felixnaredi <naredi@student.chalmers.se>
2022-10-13 20:49:09 +0200

    Added frontend error modal

---
+++ src/components/ErrorSign.vue
@@ -0,0 +1,27 @@
+<!-- eslint-disable -->
+<template>
+  <div class="p-8 bg-amber-200 border-solid border-2 border-sky-500">
+    <h1 class="text-red-500 font-bold text-lg mb-3">Error</h1>
+    <p class="mb-16">{{ message }}</p>
+    <span
+      class="
+        border-2
+        p-4
+        py-3
+        border-amber-400
+        hover:border-amber-500 hover:bg-amber-300/[.25]
+        cursor-default
+        select-none
+      "
+      >D'accueil</span
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String,
+  },
+};
+</script>
--- src/pages/game-over/App.vue
+++ src/pages/game-over/App.vue
@@ -1,16 +1,45 @@
+<!-- eslint-disable -->
 <template>
-  <score-board :score="score" :lives="lives" />
-  <div class="grid justify-items-center">
-    <div class="p-8 bg-red-500 m-4">
-      <p class="text-8xl text-white select-none">fin</p>
+  <div
+    class="
+      fixed
+      top-0
+      w-screen
+      z-20
+      grid grid-cols-12
+      mt-20
+      opacity-95
+      hover:opacity-100
+    "
+  >
+    <div class="col-span-3"></div>
+    <div class="col-span-6 place-self-center">
+      <error-sign
+        class="mt-15"
+        message="There isn't really an error. The sign is just for display."
+      />
     </div>
-    <gradient-button class="m-4" @click="newGame">
-      <div class="flex px-2">
-        <reset-arrow class="w-6 mr-4" />
-        <span>encore</span>
+    <div class="col-span-3"></div>
+  </div>
+  <div>
+    <div
+      class="fixed top-0 z-10 h-screen w-screen bg-slate-300 opacity-75"
+    ></div>
+  </div>
+  <div class="z-0">
+    <score-board :score="score" :lives="lives" />
+    <div class="grid justify-items-center">
+      <div class="p-8 bg-red-500 m-4">
+        <p class="text-8xl text-white select-none">fin</p>
       </div>
-    </gradient-button>
-    <history-table class="m-4" />
+      <gradient-button class="m-4" @click="newGame">
+        <div class="flex px-2">
+          <reset-arrow class="w-6 mr-4" />
+          <span>encore</span>
+        </div>
+      </gradient-button>
+      <history-table class="m-4" />
+    </div>
   </div>
 </template>
 
@@ -22,9 +51,16 @@ import ScoreBoard from "@/components/ScoreBoard.vue";
 import { useHistoryStore } from "@/store/history";
 import { Encoded } from "../../../dist/wasm";
 import path from "@/service/path";
+import ErrorSign from "@/components/ErrorSign.vue";
 
 export default {
-  components: { ScoreBoard, ResetArrow, HistoryTable, GradientButton },
+  components: {
+    ScoreBoard,
+    ResetArrow,
+    HistoryTable,
+    GradientButton,
+    ErrorSign,
+  },
   methods: {
     newGame() {
       window.location.href = process.env.BASE_URL;
