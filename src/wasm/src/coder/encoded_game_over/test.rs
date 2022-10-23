29c212f0c51a9149987f9300effc4c45ef419a3d
felixnaredi <naredi@student.chalmers.se>
2022-10-23 18:38:26 +0200

    String literal tests for GameOverCoderV01

--- src/wasm/src/coder/encoded_game_over/test.rs
+++ src/wasm/src/coder/encoded_game_over/test.rs
@@ -1,9 +1,14 @@
+#[cfg(test)]
+mod game_over_coder_v01;
+
+#[cfg(test)]
+mod version00coding;
+
+use std::collections::BTreeSet;
+
 use super::*;
 use crate::{
-  coder::{
-    UnseenSetID,
-    Version00Coding,
-  },
+  coder::UnseenSetID,
   game::SeenThreshold,
   game_over::GameOver,
   rng::{
@@ -32,7 +37,7 @@ fn generate_game_over(
 
   let seed = rng.next();
 
-  let unseen: Vec<String> = (0..(256 + rng.next_with_upper_bound(64)))
+  let unseen: BTreeSet<String> = (0..(256 + rng.next_with_upper_bound(64)))
     .map(|_| {
       base64::encode(
         (0..(8 + rng.next_with_upper_bound(32)))
@@ -42,6 +47,7 @@ fn generate_game_over(
       )
     })
     .collect();
+  let unseen: Vec<String> = unseen.into_iter().collect();
 
   (
     GameOver::new(
@@ -64,192 +70,6 @@ fn f()
 {
   println!(
     "{}",
-    KSINK::hash(S, "2202 TSEC 85:62:71 32 tcO nuS".as_bytes())
+    KSINK::hash(S, "2202 TSEC 61:23:81 32 tcO nuS".as_bytes())
   )
 }
-
-#[cfg(test)]
-mod version00coding
-{
-  use super::*;
-
-  #[test]
-  fn encode_decode_same_as_id()
-  {
-    let (game_over, unseen) = generate_game_over(
-      542338303675782954,
-      Some(UnseenSetID::DictionaryFr01),
-      Some(0.4.try_into().unwrap()),
-    );
-    let encoded = SealedEncodedGameOver::new::<Version00Coding, _>(&game_over).unwrap();
-    let decoded: GameOver<String> = (encoded, unseen).try_into().unwrap();
-    assert_eq!(decoded, game_over);
-  }
-
-  #[test]
-  fn encode_outputs_expected_literal()
-  {
-    let (game_over, _) = generate_game_over(
-      10607240176095180561,
-      Some(UnseenSetID::DictionaryFr01),
-      Some(0.4.try_into().unwrap()),
-    );
-
-    assert_eq!(serde_urlencoded::to_string(
-      SealedEncodedGameOver::new::<Version00Coding, _>(&game_over).unwrap()
-    )
-    .unwrap(),
-    "version=00&checksum=15367392902022108743&data=eyJ1bnNlZW5faWQiOiJEaWN0aW9uYXJ5RnIwMSIsInNlZWQiOjc3NzAzNzYyMDA2OTQ2Mjc5MTcsImluY29ycmVjdF9jb21taXRzIjpbMzMsNDksMTIxXX0%3D&unseen_set_id=DictionaryFr01")
-  }
-
-  #[test]
-  fn decode_literal_outputs_expected_game_over()
-  {
-    let s = "version=00&checksum=15965453229032169769&data=eyJ1bnNlZW5faWQiOiJEaWN0aW9uYXJ5RnIwMSIsInNlZWQiOjk3NTIwMTE1MzkzNzAxMjE1MDgsImluY29ycmVjdF9jb21taXRzIjpbMTEsMTI2LDI0OV19&unseen_set_id=DictionaryFr01";
-
-    let (game_over, unseen) = generate_game_over(
-      12837024807937892714,
-      Some(UnseenSetID::DictionaryFr01),
-      Some(0.4.try_into().unwrap()),
-    );
-
-    let decoded: GameOver<String> = (serde_urlencoded::from_str(s).unwrap(), unseen)
-      .try_into()
-      .unwrap();
-    assert_eq!(decoded, game_over);
-  }
-
-  #[test]
-  fn decode_literal_with_corrupt_checksum()
-  {
-    let (_, unseen) = generate_game_over(
-      9354512038846019256,
-      Some(UnseenSetID::DictionaryFr01),
-      Some(0.4.try_into().unwrap()),
-    );
-
-    let s = "version=00&checksum=114325682255183593&data=eyJ1bnNlZW5faWQiOiJEaWN0aW9uYXJ5RnIwMSIsInNlZWQiOjE2MTM3Mjg1Nzg3MTIwODk2OTc0LCJpbmNvcnJlY3RfY29tbWl0cyI6Wzc2LDc3LDE2MF19&unseen_set_id=DictionaryFr01";
-    assert!(matches!(
-      TryInto::<GameOver<String>>::try_into((
-        serde_urlencoded::from_str::<SealedEncodedGameOver>(s).unwrap(),
-        unseen
-      )),
-      Err(_)
-    ));
-  }
-
-  #[test]
-  fn decode_literal_with_corrupt_data()
-  {
-    let (_, unseen) = generate_game_over(
-      3088894222515458658,
-      Some(UnseenSetID::DictionaryFr01),
-      Some(0.4.try_into().unwrap()),
-    );
-
-    let s = "version=00&checksum=15649906318563636177&data=eyJ1bnNlZW5faWQiOiJEaWN0aW9uYXJ5RnIwMSIsInNlZWQiOjE5MzUyNTQ2NDY0MTkzOTI1NzYsImluY29ycmVjdF9jb21taXRzIjpbMTQsMTUsMTUzXX0%3D&unseen_set_id=DictionaryFr01";
-    assert!(matches!(
-      TryInto::<GameOver<String>>::try_into((
-        serde_urlencoded::from_str::<SealedEncodedGameOver>(s).unwrap(),
-        unseen
-      )),
-      Err(_)
-    ));
-  }
-
-  #[test]
-  fn decode_url_with_missing_fields()
-  {
-    let (game_over, unseen) = generate_game_over(
-      17561082018945952039,
-      Some(UnseenSetID::DictionaryFr01),
-      Some(0.4.try_into().unwrap()),
-    );
-
-    let version = "version=00";
-    let checksum = "checksum=16916324704992223141";
-    let data = "data=eyJ1bnNlZW5faWQiOiJEaWN0aW9uYXJ5RnIwMSIsInNlZWQiOjcxMzA5MDM0Nzg0NDc2NzAwMjUsImluY29ycmVjdF9jb21taXRzIjpbMTMxLDE3NiwyNDFdfQ%3D%3D";
-    let unseen_set_id = "unseen_set_id=DictionaryFr01";
-
-    for i in 0..15 {
-      let fields: Vec<String> = [
-        (i & 1 != 0).then(|| version),
-        (i & 2 != 0).then(|| checksum),
-        (i & 4 != 0).then(|| data),
-        (i & 8 != 0).then(|| unseen_set_id),
-      ]
-      .iter()
-      .filter_map(|x| x.map(|x| x.into()))
-      .collect();
-      let fields = fields.join("&");
-
-      if i == 7 {
-        // All fields but `unseen_set_id` is present. This is a valid URL.
-        let decoded: GameOver<String> =
-          (serde_urlencoded::from_str(&fields).unwrap(), unseen.clone())
-            .try_into()
-            .unwrap();
-        assert_eq!(decoded, game_over);
-      } else {
-        // Invalid URL. This will be detected when deserializing the URL into a
-        // `SealedEncodedGameOver`
-        assert!(matches!(
-          serde_urlencoded::from_str::<SealedEncodedGameOver>(&fields),
-          Err(_)
-        ));
-      }
-    }
-  }
-}
-
-/*
-mod game_over_coder_v01
-{
-  use std::iter;
-
-  use crate::{
-    coder::{
-      GameOverCoderV01,
-      SealedEncodedGameOver,
-      UnseenSetID,
-    },
-    game_over::GameOver,
-  };
-
-  #[test]
-  fn encode_decode_equals_id()
-  {
-    let unseen: Vec<[u8; 1]> = (0..64).map(|x| [x]).collect();
-    let game_over = GameOver::new(
-      9940370477626720397,
-      UnseenSetID::Unspecified,
-      unseen.clone(),
-      0.4.try_into().unwrap(),
-      [Some(9), Some(15), Some(35)],
-    );
-    let encoded = SealedEncodedGameOver::new::<GameOverCoderV01, _>(game_over.clone()).unwrap();
-    let decoded = GameOver::try_from((encoded, unseen)).unwrap();
-
-    assert_eq!(decoded.element_checksum(), game_over.element_checksum());
-    assert_eq!(decoded.score(), game_over.score());
-    assert_eq!(decoded.lives(), game_over.lives());
-    assert!(iter::zip(decoded.into_iter(), game_over.into_iter()).all(|(x, y)| x == y));
-  }
-
-  #[test]
-  fn detect_use_of_wrong_set_when_decoding()
-  {
-    let e = SealedEncodedGameOver::new::<GameOverCoderV01, _>(GameOver::new(
-      622451429113938556,
-      UnseenSetID::Unspecified,
-      // The unseen set used when encoding...
-      (0..64).map(|x| [x]).collect(),
-      0.4.try_into().unwrap(),
-      [Some(30), Some(31), Some(54)],
-    ))
-    .unwrap();
-
-    assert!(GameOver::try_from((e, (64..128).map(|x| [x]).collect())).is_err());
-  }
-}
-*/
---
+++ src/wasm/src/coder/encoded_game_over/test/game_over_coder_v01.rs
@@ -0,0 +1,145 @@
+use super::*;
+
+#[test]
+fn encode_decode_same_as_id()
+{
+  let (game_over, unseen) = generate_game_over(9239737542598549709, None, None);
+  let encoded = SealedEncodedGameOver::new::<GameOverCoderV01, _>(&game_over).unwrap();
+  let decoded: GameOver<String> = (encoded, unseen).try_into().unwrap();
+  assert_eq!(decoded, game_over);
+}
+
+#[test]
+fn encode_outputs_expected_literal()
+{
+  let (game_over, _) = generate_game_over(12504334248584760776, None, None);
+  assert_eq!(serde_urlencoded::to_string(
+     SealedEncodedGameOver::new::<GameOverCoderV01, _>(&game_over).unwrap()
+   )
+   .unwrap(),
+   "version=goc-v01&checksum=6196960203795175085&data=eyJzZWVkIjoyMDg5MDM0NDI0NzI0NTg3NDI4LCJzZWVuX3RocmVzaG9sZCI6NzQ0NTY5Nzg4LCJpbmNvcnJlY3RfY29tbWl0cyI6WzEwMCwyMTYsMjI5XSwiZWxlbWVudF9jaGVja3N1bSI6NjYzODY2NTM4MjQzMzEyNDAwMn0%3D&unseen_set_id=Unspecified"
+   );
+}
+
+#[test]
+fn decode_literal_outputs_expected_game_over()
+{
+  let s = "version=goc-v01&checksum=9581572683804129452&data=eyJzZWVkIjoxODI0NzEyNTcwNzA3Mjk1MTMxLCJzZWVuX3RocmVzaG9sZCI6NDU0MjQwOTI3LCJpbmNvcnJlY3RfY29tbWl0cyI6WzEyNiwxODYsMTk1XSwiZWxlbWVudF9jaGVja3N1bSI6MTU2MTYzNDEyNzMzOTc2OTgzMzh9&unseen_set_id=Unspecified";
+  let (game_over, unseen) = generate_game_over(5342632399507448204, None, None);
+
+  let decoded: GameOver<String> = (serde_urlencoded::from_str(s).unwrap(), unseen)
+    .try_into()
+    .unwrap();
+  assert_eq!(decoded, game_over);
+}
+
+#[test]
+fn decode_literal_with_corrupt_checksum()
+{
+  let (_, unseen) = generate_game_over(4405435083803308078, None, None);
+  let s = "version=goc-v01&checksum=3417369460210627685&data=eyJzZWVkIjoxMTQ2MjY4NDcyNTcyMDU1MTM2Nywic2Vlbl90aHJlc2hvbGQiOjE3MzYzMDgxOSwiaW5jb3JyZWN0X2NvbW1pdHMiOlsxLDg1LDIwMV0sImVsZW1lbnRfY2hlY2tzdW0iOjk1NjE2Mzk2OTIzMDU4NzMxMDV9&unseen_set_id=Unspecified";
+  assert!(matches!(
+    TryInto::<GameOver<String>>::try_into((
+      serde_urlencoded::from_str::<SealedEncodedGameOver>(s).unwrap(),
+      unseen
+    )),
+    Err(_)
+  ));
+}
+
+#[test]
+fn decode_literal_with_corrupt_data()
+{
+  let (_, unseen) = generate_game_over(16739628093234127117, None, None);
+  let s = "version=goc-v01&checksum=5597641129812305779&data=eyJzZWVkIjoxMjI3OTE5OTkyMTE2MjkyNjA2MCwic2Vlbl90aHJlc2hvbGQiOjQ0OTc2MTMyMywiaW5jb3JyZWN0X2NvbW1pdHMiOls4OSwyMDEsMjU0XSwiZWxlbWVudF9jaGVja3N1bSI6MTgzNDI4MzQyNzM0MDA4OTYxMjl9&unseen_set_id=Unspecified";
+  assert!(matches!(
+    TryInto::<GameOver<String>>::try_into((
+      serde_urlencoded::from_str::<SealedEncodedGameOver>(s).unwrap(),
+      unseen
+    )),
+    Err(_)
+  ));
+}
+
+#[test]
+fn decode_url_with_missing_fields()
+{
+  let (game_over, unseen) = generate_game_over(10012246512391581010, None, None);
+
+  println!(
+    "{}",
+    serde_urlencoded::to_string(
+      SealedEncodedGameOver::new::<GameOverCoderV01, _>(&game_over).unwrap()
+    )
+    .unwrap()
+  );
+
+  let version = "version=goc-v01";
+  let checksum = "checksum=13561387729840425778";
+  let data = "data=eyJzZWVkIjoxNTc2NDEzMzM4NzA1NzY2MzQwLCJzZWVuX3RocmVzaG9sZCI6NTYzODM1ODk4LCJpbmNvcnJlY3RfY29tbWl0cyI6WzIyLDIzLDg2XSwiZWxlbWVudF9jaGVja3N1bSI6MTgyNzI1NzMwNzg1Njk3Nzk3MTJ9";
+  let unseen_set_id = "unseen_set_id=Unspecified";
+
+  for i in 0..15 {
+    let fields: Vec<String> = [
+      (i & 1 != 0).then(|| version),
+      (i & 2 != 0).then(|| checksum),
+      (i & 4 != 0).then(|| data),
+      (i & 8 != 0).then(|| unseen_set_id),
+    ]
+    .iter()
+    .filter_map(|x| x.map(|x| x.into()))
+    .collect();
+    let fields = fields.join("&");
+
+    if i == 7 {
+      // All fields but `unseen_set_id` is present. This will not be detected when deserializing the
+      // URL into a `SealedEncodedGameOver` but when decoding into a `GameOver`.
+      assert!(matches!(
+        TryInto::<GameOver<String>>::try_into((
+          serde_urlencoded::from_str::<SealedEncodedGameOver>(&fields).unwrap(),
+          unseen.clone()
+        )),
+        Err(_)
+      ));
+    } else {
+      // Invalid URL. This will be detected when deserializing the URL into a
+      // `SealedEncodedGameOver`
+      assert!(matches!(
+        serde_urlencoded::from_str::<SealedEncodedGameOver>(&fields),
+        Err(_)
+      ));
+    }
+  }
+}
+
+#[test]
+fn decode_url_with_wrong_unseen_set_id()
+{
+  let (_, unseen) = generate_game_over(
+    10030715364591628463,
+    Some(UnseenSetID::Top999WiktionaryFr),
+    None,
+  );
+  let s = "version=goc-v01&checksum=1487839826841008537&data=eyJzZWVkIjo5ODE2MDU4Mzg1MzIzNzUxMjMzLCJzZWVuX3RocmVzaG9sZCI6NDE3MTk1NzkzLCJpbmNvcnJlY3RfY29tbWl0cyI6WzE1MSwyMTMsMjU0XSwiZWxlbWVudF9jaGVja3N1bSI6MzMyMTQwODUzNDE4MzM0NzQwOH0%3D&unseen_set_id=DictionaryFr01";
+  assert!(matches!(
+    TryInto::<GameOver<String>>::try_into((
+      serde_urlencoded::from_str::<SealedEncodedGameOver>(&s).unwrap(),
+      unseen.clone()
+    )),
+    Err(_)
+  ));
+}
+
+#[test]
+fn decode_url_with_modified_unseen()
+{
+  let (_, unseen) = generate_game_over(2657328141259618373, None, None);
+  let s = "version=goc-v01&checksum=5106451020992070949&data=eyJzZWVkIjoxODMwMDExNjgxODczODIzODI5LCJzZWVuX3RocmVzaG9sZCI6NDAwNTAxMDM1LCJpbmNvcnJlY3RfY29tbWl0cyI6Wzc0LDE4MiwyMjNdLCJlbGVtZW50X2NoZWNrc3VtIjo5MzY4NTg2ODYzNjQxMDM0NTk2fQ%3D%3D&unseen_set_id=Unspecified";
+  assert!(matches!(
+    TryInto::<GameOver<String>>::try_into((
+      serde_urlencoded::from_str::<SealedEncodedGameOver>(&s).unwrap(),
+      unseen.into_iter().rev().collect()
+    )),
+    Err(_)
+  ));
+}
