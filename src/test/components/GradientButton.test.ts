33a245798685c3a2c227ca713cc938e670246f4f
felixnaredi <naredi@student.chalmers.se>
2022-10-24 01:04:34 +0200

    Added test for GradientButton and made version-goc-v01.test.ts silent its expected error messages to the console

--- src/test/components/GradientButton.test.ts
+++ src/test/components/GradientButton.test.ts
@@ -7,13 +7,20 @@ import { VueWrapper, mount } from "@vue/test-utils";
 
 const TEXT = "GHCi, version 9.4.2";
 
-function mountGradientButton() {
+function mountGradientButton(): VueWrapper<any> {
   return mount(GradientButton, {
     slots: {
       default: `<p>${TEXT}</p>`,
     },
   });
 }
+
+test("check that the button emits 'click' when clicked", async () => {
+  const wrapper = mountGradientButton();
+  wrapper.find("button").trigger("click");
+  await wrapper.vm.$nextTick();
+  expect(wrapper.emitted().click).toBeTruthy();
+});
 
 test("check that the button slot contains what is expected", () => {
   const wrapper = mountGradientButton();
